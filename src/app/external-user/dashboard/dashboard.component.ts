e7f412826b5ca371848bb2caa047cc9d8a12c119
PragnaMunagala <pmunagal@asu.edu>
2017-10-21 10:41:07 -0700

    Dashboard and external user UI changes

--- src/app/external-user/dashboard/dashboard.component.ts
+++ src/app/external-user/dashboard/dashboard.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
 
+
+export class DashboardComponent implements OnInit {
+  public headerData = [];
+  public tableData = [];
   constructor() { }
 
+
   ngOnInit() {
+    this.headerData = ['Date', 'Description', 'Type', 'Amount', 'Balance'];
+    this.tableData = [
+      ['2017-10-19', 'Discover E-Payment', 'ACH Debit', '$100', '$1345'],
+      ['2017-10-15', 'ASU Direct Deposit', 'ACH Credit', '$100', '$1445'],
+    ];
   }
 
 }
